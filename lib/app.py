d6be92a0b9fe8ea0a854145abad5a4e4c84894c3
Michael <oliengo.michael@gmail.com>
2024-06-27 18:09:30 +0300

    complete functions

---
+++ lib/app.py
@@ -0,0 +1,17 @@
+# def my_function(param):
+#     print("Running my_function")
+#     return param + 1
+
+# # Call the function 
+# my_function_return_value = my_function(1)
+
+# # Print the return value
+# print(my_function_return_value)
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+stylish_painter()
--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+# Call the function with no arguments
+greet_with_default()
+
+# Call the function with an argument
+greet_with_default("Naureen")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+# Call the function with two arguments and print the result
+sum_value = add(1, 2)
+print(sum_value)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
+
+# Test cases
+result1 = halve(4)
+print(result1)  # Output: 2.0
+
+result2 = halve("two")
+print(result2)  # Output: None
+
